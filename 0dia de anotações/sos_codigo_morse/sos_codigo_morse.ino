7f283feea73c7d400ab3e94aea686904c765a34e
mateus52479 <mateus.248123@alunosatc.edu.br>
2025-06-09 20:25:04 -0300

    Update sos_codigo_morse.ino

--- 0dia de anotações/sos_codigo_morse/sos_codigo_morse.ino
+++ 0dia de anotações/sos_codigo_morse/sos_codigo_morse.ino
@@ -1,24 +1,47 @@
-#define pin_led1 8
-#define pin_led2 9
-#define pin_led3 10
-#define pin_led4 11
+#define pin_led1 13
 void setup() {
 
 pinMode(pin_led1, OUTPUT);
-pinMode(pin_led2, OUTPUT);
-pinMode(pin_led3, OUTPUT);
-pinMode(pin_led4, OUTPUT);
 }
 
 void loop() {
-digitalWrite(pin_led1, HIGH);
-digitalWrite(pin_led3, HIGH);
-digitalWrite(pin_led2, LOW);
-digitalWrite(pin_led4, LOW);
-delay(1000);
-digitalWrite(pin_led1, LOW);
-digitalWrite(pin_led3, LOW);
-digitalWrite(pin_led2, HIGH);
-digitalWrite(pin_led4, HIGH);
-delay(1000);
+
+digitalWrite(pin_led1, HIGH); 
+delay(200); 
+digitalWrite(pin_led1, LOW); 
+delay(200); 
+digitalWrite(pin_led1, HIGH); 
+delay(200); 
+digitalWrite(pin_led1, LOW); 
+delay(200); 
+digitalWrite(pin_led1, HIGH); 
+delay(200); 
+digitalWrite(pin_led1, LOW); 
+delay(200); 
+
+digitalWrite(pin_led1, HIGH); 
+delay(400); 
+digitalWrite(pin_led1, LOW); 
+delay(400); 
+digitalWrite(pin_led1, HIGH); 
+delay(400); 
+digitalWrite(pin_led1, LOW); 
+delay(400); 
+digitalWrite(pin_led1, HIGH); 
+delay(400); 
+digitalWrite(pin_led1, LOW); 
+delay(400); 
+
+digitalWrite(pin_led1, HIGH); 
+delay(200); 
+digitalWrite(pin_led1, LOW); 
+delay(200); 
+digitalWrite(pin_led1, HIGH); 
+delay(200); 
+digitalWrite(pin_led1, LOW); 
+delay(200); 
+digitalWrite(pin_led1, HIGH); 
+delay(200); 
+digitalWrite(pin_led1, LOW); 
+delay(1000); 
 }
