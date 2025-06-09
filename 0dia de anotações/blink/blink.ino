7038b03e36617ca9a153b97445f5c4dff08cb848
mateus52479 <mateus.248123@alunosatc.edu.br>
2025-06-09 20:19:21 -0300

    Add files via upload
    anotações antes da indicação de postagem no github

---
+++ 0dia de anotações/blink/blink.ino
@@ -0,0 +1,47 @@
+#define pin_led1 13
+void setup() {
+
+pinMode(pin_led1, OUTPUT);
+}
+
+void loop() {
+
+digitalWrite(pin_led1, HIGH); 
+delay(200); 
+digitalWrite(pin_led1, LOW); 
+delay(200); 
+digitalWrite(pin_led1, HIGH); 
+delay(200); 
+digitalWrite(pin_led1, LOW); 
+delay(200); 
+digitalWrite(pin_led1, HIGH); 
+delay(200); 
+digitalWrite(pin_led1, LOW); 
+delay(200); 
+
+digitalWrite(pin_led1, HIGH); 
+delay(400); 
+digitalWrite(pin_led1, LOW); 
+delay(400); 
+digitalWrite(pin_led1, HIGH); 
+delay(400); 
+digitalWrite(pin_led1, LOW); 
+delay(400); 
+digitalWrite(pin_led1, HIGH); 
+delay(400); 
+digitalWrite(pin_led1, LOW); 
+delay(400); 
+
+digitalWrite(pin_led1, HIGH); 
+delay(200); 
+digitalWrite(pin_led1, LOW); 
+delay(200); 
+digitalWrite(pin_led1, HIGH); 
+delay(200); 
+digitalWrite(pin_led1, LOW); 
+delay(200); 
+digitalWrite(pin_led1, HIGH); 
+delay(200); 
+digitalWrite(pin_led1, LOW); 
+delay(1000); 
+}
